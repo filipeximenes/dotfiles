9f4961b74a994ac7ff985c07cccb23548429726b
Filipe Ximenes <filipeximenes@gmail.com>
2013-12-13 13:19:35 -0300

    removing cask directory

--- Casks/harvest11.rb
+++
@@ -1,7 +0,0 @@
-class Harvest < Cask
-  url 'http://www.getharvest.com/harvest/mac/Harvest.1.1.zip'
-  homepage 'http://www.getharvest.com/mac'
-  version '1.1'
-  no_checksum
-  link 'Harvest.app'
-end
