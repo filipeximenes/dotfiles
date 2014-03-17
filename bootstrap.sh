05bbadadd0781f3948bf36451cfd2a0d191a93f1
Filipe Ximenes <filipeximenes@gmail.com>
2014-03-17 00:20:42 -0300

    moving mac fresh install and adding sublime sync

--- bootstrap.sh
+++ bootstrap.sh
@@ -3,7 +3,8 @@ cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
-		--exclude "README.md" --exclude "LICENSE-MIT.txt" -av --no-perms . ~
+		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "mac_fresh_install.sh" \
+		--exclude "sync_sublime.sh" -av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -18,7 +19,7 @@ fi
 unset doIt
 
 if [[ "$OSTYPE" == 'linux-gnu' ]]; then
-    cp ~/.sublime_conf/* ~/.config/sublime-text-2/Packages/User/
+    cp -r ~/.sublime_conf/* ~/.config/sublime-text-2/Packages/User/
 else
-    cp ~/.sublime_conf/* ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+    cp -r ~/.sublime_conf/* ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 fi
---
+++ sync_sublime.sh
@@ -0,0 +1,2 @@
+
+cp -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/* ~/.sublime_conf/
