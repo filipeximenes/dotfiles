c841ec55fb9c4a45d870c4f1da4de6ee5c2dd682
Filipe Ximenes <filipeximenes@gmail.com>
2014-03-17 00:31:43 -0300

    excluding .sublime_conf folder from rsync

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,7 +4,7 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "mac_fresh_install.sh" \
-		--exclude "sync_sublime.sh" -av --no-perms . ~
+		--exclude "sync_sublime.sh" --exclude=".sublime_conf/" -av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -19,7 +19,7 @@ fi
 unset doIt
 
 if [[ "$OSTYPE" == 'linux-gnu' ]]; then
-    cp ~/.sublime_conf/* ~/.config/sublime-text-2/Packages/User/
+    cp .sublime_conf/* ~/.config/sublime-text-2/Packages/User/
 else
-    cp ~/.sublime_conf/* ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
+    cp .sublime_conf/* ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
 fi
