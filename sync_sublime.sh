9cf99b5674f62a6d91f3357e5b738039d15d038b
Filipe Ximenes <filipeximenes@gmail.com>
2014-03-17 00:35:09 -0300

    adding linux version of sublime sync

--- sync_sublime.sh
+++ sync_sublime.sh
@@ -1,2 +1,6 @@
 
-cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/* .sublime_conf/
+if [[ "$OSTYPE" == 'linux-gnu' ]]; then
+    cp ~/.config/sublime-text-2/Packages/User/* .sublime_conf/
+else
+    cp ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/* .sublime_conf/
+fi
