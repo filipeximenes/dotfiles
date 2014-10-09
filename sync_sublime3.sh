545fa5ff09e9166446273475f9f9664cc0268f3a
Filipe Ximenes <filipeximenes@gmail.com>
2014-10-09 13:26:17 -0300

    adding sublime 3 installation configurations

---
+++ sync_sublime3.sh
@@ -0,0 +1,6 @@
+
+if [[ "$OSTYPE" == 'linux-gnu' ]]; then
+    rsync -cp -r ~/.config/sublime-text-2/Packages/User/* .sublime3_conf/
+else
+    rsync -cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/* .sublime3_conf/
+fi
