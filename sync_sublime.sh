92c2e13db78dcfa982664301a071e4cff255d1d6
Filipe Ximenes <filipeximenes@gmail.com>
2017-08-21 19:00:57 -0300

    updates sublime configuration

--- sync_sublime.sh
+++
@@ -1,6 +0,0 @@
-
-if [[ "$OSTYPE" == 'linux-gnu' ]]; then
-    rsync -cp -r ~/.config/sublime-text-2/Packages/User/* .sublime_conf/
-else
-    rsync -cp -r ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User/* .sublime_conf/
-fi
