573d5da891671838dc7d5aa8ae681cf47890b9ca
Filipe A Ximenes <filipeximenes@gmail.com>
2017-08-24 11:39:13 -0300

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,8 +4,7 @@ git pull origin master
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" \
 		--exclude "README.md" --exclude "LICENSE-MIT.txt" --exclude "mac_fresh_install.sh" \
-		--exclude "sync_sublime3.sh" --exclude "sync_sublime.sh" --exclude=".sublime_conf/" \
-		--exclude=".sublime3_conf/" -av --no-perms . ~
+		--exclude "sync_sublime3.sh" --exclude=".sublime3_conf/" -av --no-perms . ~
 	source ~/.bash_profile
 }
 if [ "$1" == "--force" -o "$1" == "-f" ]; then
@@ -18,12 +17,6 @@ else
 	fi
 fi
 unset doIt
-
-if [[ "$OSTYPE" == 'linux-gnu' ]]; then
-    cp .sublime_conf/* ~/.config/sublime-text-2/Packages/User/
-else
-    cp .sublime_conf/* ~/Library/Application\ Support/Sublime\ Text\ 2/Packages/User
-fi
 
 
 if [[ "$OSTYPE" == 'linux-gnu' ]]; then
