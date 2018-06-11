016d2d65a8b6d2b3545bbd3be4443e5d3391b7ae
Filipe Ximenes <filipeximenes@gmail.com>
2018-06-11 16:21:03 -0300

    removes w3c validator

--- sync_sublime3.sh
+++ sync_sublime3.sh
@@ -2,9 +2,7 @@
 if [[ "$OSTYPE" == 'linux-gnu' ]]; then
     rsync -cp -r ~/.config/sublime-text-3/Packages/User/*.sublime-settings .sublime3_conf/
     rsync -cp -r ~/.config/sublime-text-3/Packages/User/*.sublime-keymap .sublime3_conf/
-    rsync -cp -r ~/.config/sublime-text-3/Packages/User/*.py .sublime3_conf/
 else
     rsync -cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/*.sublime-settings .sublime3_conf/
     rsync -cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/*.sublime-keymap .sublime3_conf/
-    rsync -cp -r ~/Library/Application\ Support/Sublime\ Text\ 3/Packages/User/*.py .sublime3_conf/
 fi
